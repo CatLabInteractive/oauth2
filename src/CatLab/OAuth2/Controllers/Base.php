4cdee7203f27c096f7c1e611667895463c354bb1
Thijs Van der Schaeghe <thijs@catlab.be>
2015-02-19 01:35:37 +0100

    Updating oauth2 library to 1.6.

--- src/CatLab/OAuth2/Controllers/Base.php
+++ src/CatLab/OAuth2/Controllers/Base.php
@@ -30,7 +30,7 @@ abstract class Base
 	 */
 	public function __construct (\Neuron\Interfaces\Module $module = null)
 	{
-		if (! ($module instanceof Module))
+		if (! ($module instanceof Base))
 		{
 			throw new InvalidParameter ("Controller must be instanciated with a \\CatLab\\OAuth\\Module. Instance of " . get_class ($module) . " given.");
 		}
