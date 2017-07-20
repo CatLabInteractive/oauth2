b879f70e702e892be2630335aa17effcf849faac
Thijs Van der Schaeghe <thijs@catlab.be>
2017-07-20 15:43:18 +0200

    Add support for wildcard redirect domains.

--- src/CatLab/OAuth2/Verifier.php
+++ src/CatLab/OAuth2/Verifier.php
@@ -6,59 +6,77 @@ use CatLab\OAuth2\Models\OAuth2Service;
 use Neuron\Exceptions\InvalidParameter;
 use Neuron\Net\Request;
 
+/**
+ * Class Verifier
+ * @package CatLab\OAuth2
+ */
 class Verifier
 {
-	private static function getInstance ()
-	{
-		static $in;
-		if (!isset ($in))
-		{
-			$in = new self ();
-		}
-		return $in;
-	}
+    private static function getInstance()
+    {
+        static $in;
+        if (!isset ($in)) {
+            $in = new self ();
+        }
+        return $in;
+    }
 
-	private function __construct ()
-	{
+    private function __construct()
+    {
 
-	}
+    }
 
-	/** @var int */
-	private $userid;
-	private $token;
+    /** @var int */
+    private $userid;
 
-	public static function isValid (Request $request)
-	{
-		$in = self::getInstance ();
+    /** @var string */
+    private $token;
 
-		$oauth2request = OAuth2Service::getInstance ()->translateRequest ($request);
-		$valid = OAuth2Service::getInstance ()->getServer ()->verifyResourceRequest ($oauth2request);
+    public static function isValid(Request $request)
+    {
+        $oauth2request = OAuth2Service::getInstance()->translateRequest($request);
+        $valid = OAuth2Service::getInstance()->getServer()->verifyResourceRequest($oauth2request);
 
-		if ($valid)
-		{
-			$data = OAuth2Service::getInstance ()->getServer ()->getAccessTokenData ($oauth2request);
+        if ($valid) {
+            $data = OAuth2Service::getInstance()->getServer()->getAccessTokenData($oauth2request);
+            self::setUser($data['access_token'], $data['user_id']);
+        }
 
-			$in->token = $data['access_token'];
-			$in->userid = $data['user_id'];
-		}
+        return $valid;
+    }
 
-		return $valid;
-	}
+    /**
+     * @param $accessToken
+     * @param $userId
+     */
+    public static function setUser($accessToken, $userId)
+    {
+        self::getInstance()->token = $accessToken;
+        self::getInstance()->userid = $userId;
+    }
 
-	public static function getUserId ()
-	{
-		if (!isset (self::getInstance ()->userid))
-			throw new InvalidParameter ("isValid was not called, cannot get user id.");
+    /**
+     * @return int
+     * @throws InvalidParameter
+     */
+    public static function getUserId()
+    {
+        if (!isset (self::getInstance()->userid))
+            throw new InvalidParameter ("isValid was not called, cannot get user id.");
 
-		return self::getInstance ()->userid;
-	}
+        return self::getInstance()->userid;
+    }
 
-	public static function getAccessToken ()
-	{
-		if (!isset (self::getInstance ()->token))
-			throw new InvalidParameter ("isValid was not called, cannot get access token.");
+    /**
+     * @return string
+     * @throws InvalidParameter
+     */
+    public static function getAccessToken()
+    {
+        if (!isset (self::getInstance()->token))
+            throw new InvalidParameter ("isValid was not called, cannot get access token.");
 
-		return self::getInstance ()->token;
-	}
+        return self::getInstance()->token;
+    }
 
 }
